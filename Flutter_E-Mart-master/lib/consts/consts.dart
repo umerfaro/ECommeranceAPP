801766747679dd66f5608966080e84b53e7228a2
Umer farooq <i211184@nu.edu.pk>
2024-01-18 19:11:11 +0500

    firebase setup

--- Flutter_E-Mart-master/lib/consts/consts.dart
+++ Flutter_E-Mart-master/lib/consts/consts.dart
@@ -3,5 +3,6 @@ export './colors.dart';
 export './images.dart';
 export './strings.dart';
 export './styles.dart';
+export './FireBase_const.dart';
 export 'package:velocity_x/velocity_x.dart';
 export 'package:flutter/material.dart';
--- Flutter_E-Mart-master/lib/main.dart
+++ Flutter_E-Mart-master/lib/main.dart
@@ -1,9 +1,33 @@
-import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'Views/SplashScreen/SplashScreen.dart';
 import 'consts/consts.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if(kIsWeb)
+  {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyBnhtKILH_8AKYGDvwkmuNKmz1hlJq6kJM",
+            authDomain: "metastore-41b1a.firebaseapp.com",
+            projectId: "metastore-41b1a",
+            storageBucket: "metastore-41b1a.appspot.com",
+            messagingSenderId: "336284957113",
+            appId: "1:336284957113:web:e5a06eaff9387a09b41e68",
+            measurementId: "G-VE6DS97BJ5"
+
+        ));
+  }
+  else{
+    await Firebase.initializeApp(
+    );
+  }
+
+
+
   runApp(const MyApp());
 }
 
